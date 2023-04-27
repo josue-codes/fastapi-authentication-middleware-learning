206f748ca244cba55d31f296e4a98574a72cc540
Joshua Andujar <josue@josue.codes>
2023-04-26 23:35:14 -0400

    trying this out

---
+++ main.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+import time
+import pathlib
+from typing import Dict
+
+from fastapi import (
+    Depends,
+    FastAPI,
+    HTTPException,
+    Request,
+    Response
+)
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+from src.auth import (
+    authenticate_api_key,
+    authenticate_auth_token,
+    generate_auth_token,
+)
+
+from src.config import Config
+
+
+STATIC_DIRECTORY = pathlib.Path(__file__).parent / 'static'
+
+
+APP = FastAPI()
+AUTH_TOKENS: Dict[str, float] = {}
+
+
+for file_sys_obj in STATIC_DIRECTORY.iterdir():
+    if file_sys_obj.is_file():
+        continue
+    APP.mount(
+        path=f'/{file_sys_obj.name}',
+        app=StaticFiles(directory=file_sys_obj.absolute()),
+        name=file_sys_obj.name
+    )
+
+
+# APP.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=['*'],
+#     allow_credentials=True,
+#     allow_methods=['*'],
+#     allow_headers=['*'],
+# )
+
+
+@APP.post('/auth/token')
+async def authenticate_user(api_key: bool = Depends(authenticate_api_key)):
+    if api_key:
+        auth_token = generate_auth_token()
+        AUTH_TOKENS[auth_token] = time.time()
+        return {'auth_token': auth_token}
+    raise HTTPException(status_code=401, detail='Unauthorized')
+
+
+@APP.middleware('https')
+async def auth_middleware(request: Request, call_next):
+    if request.url.path == '/auth/token':
+        response = await call_next(request)
+    else:
+        if authenticate_auth_token(request, AUTH_TOKENS):
+            response = await call_next(request)
+        else:
+            response = Response(status_code=401, content='Unauthorized')
+
+    return response
+
+
+@APP.get('/secure/data')
+async def get_secure_data():
+    return {"data": "This is secure data"}
+
+
+if __name__ == '__main__':  # pragma: no cover
+    uvicorn.run(
+        app=APP,
+        host='0.0.0.0',
+        port=443,
+        ssl_certfile=Config.ssl_certfile,
+        ssl_keyfile=Config.ssl_keyfile
+    )
---
+++ src/auth.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from uuid import uuid4
+import hashlib
+import time
+
+from fastapi.security import APIKeyHeader
+
+from src.config import Config
+
+api_key_header = APIKeyHeader(name="api_key", auto_error=False)
+
+
+def authenticate_api_key(api_key: str | None) -> bool:
+    return api_key == Config.api_key
+
+
+def generate_auth_token() -> str:
+    token = str(uuid4())
+    hashed_token = hashlib.sha256(token.encode()).hexdigest()
+    return hashed_token
+
+
+def authenticate_auth_token(request, auth_tokens):
+    auth_header = request.headers.get("Authorization")
+
+    if not auth_header:
+        return False
+    auth_token = auth_header.split(" ")[-1]
+    if auth_token not in auth_tokens:
+        return False
+    token_age = time.time() - auth_tokens[auth_token]
+    return token_age < 28800  # 8 hours
+
+
+if __name__ == '__main__':  # pragma: no cover
+    pass
