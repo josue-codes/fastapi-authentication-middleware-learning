37f701d1030aa2d1991b5bc0bd6f88cc274bfe51
Joshua Andujar <josue@josue.codes>
2023-05-11 22:06:27 -0400

    updating config to match needed settings

--- src/config.py
+++ src/config.py
@@ -3,11 +3,34 @@ import os
 
 class Config:
     @property
-    def api_key(self) -> str:
-        key = 'API_KEY'
-        if os.getenv(key):
-            return os.getenv(key)
-        raise EnvironmentError(f'{key} environment variable not set.')
+    def secret_key(self) -> str:
+        return get_env('SECRET_KEY')
+
+    @property
+    def db_username(self) -> str:
+        return get_env('DB_USERNAME')
+
+    @property
+    def db_password(self) -> str:
+        return get_env('DB_PASSWORD')
+
+    @property
+    def db_project_name(self) -> str:
+        return get_env('DB_PROJECT_NAME')
+
+    @property
+    def db_name(self) -> str:
+        return get_env('DB_NAME')
+
+    @property
+    def db_collection_name(self) -> str:
+        return get_env('DB_COLLECTION_NAME')
+
+
+def get_env(env_name: str) -> str:
+    if os.getenv(env_name):
+        return os.getenv(env_name)
+    raise EnvironmentError(f'{env_name} environment variable not set.')
 
 
 if __name__ == '__main__':  # pragma: no cover
