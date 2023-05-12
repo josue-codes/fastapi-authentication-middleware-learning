6a919b38f1a4f797580df34d73131f0c4ec4497d
Joshua Andujar <josue@josue.codes>
2023-05-11 22:13:33 -0400

    removing a bunch of unneeded stuff

--- src/auth.py
+++ src/auth.py
@@ -1,36 +1,22 @@
 from __future__ import annotations
 
-from uuid import uuid4
-import hashlib
-import time
+import os
+import datetime
 
-from fastapi.security import APIKeyHeader
-
-from src.config import Config
-
-api_key_header = APIKeyHeader(name="api_key", auto_error=False)
+import jwt
 
 
-def authenticate_api_key(api_key: str | None) -> bool:
-    return api_key == Config().api_key
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = 'HS256'
+AUTH_TOKEN_EXPIRE_MINUTES = 600
 
 
-def generate_auth_token() -> str:
-    token = str(uuid4())
-    hashed_token = hashlib.sha256(token.encode()).hexdigest()
-    return hashed_token
-
-
-def authenticate_auth_token(request, auth_tokens):
-    auth_header = request.headers.get("Authorization")
-
-    if not auth_header:
-        return False
-    auth_token = auth_header.split(" ")[-1]
-    if auth_token not in auth_tokens:
-        return False
-    token_age = time.time() - auth_tokens[auth_token]
-    return token_age < 28800  # 8 hours
+def generate_auth_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=AUTH_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 if __name__ == '__main__':  # pragma: no cover
