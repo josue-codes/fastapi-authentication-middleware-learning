d9718a464f2d3aa8c5efe11e93900641efeacb36
Joshua Andujar <josue@josue.codes>
2023-05-11 22:14:32 -0400

    swapping to username / password combo for login. not exactly blocking other routes at this moment but headed in the right direction.

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from __future__ import annotations
 
-import time
+import hashlib
 import pathlib
 from typing import Dict
 
 from fastapi import (
-    Depends,
     FastAPI,
     HTTPException,
-    Request,
-    Response
+    status
 )
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 import uvicorn
 
-from src.auth import (
-    authenticate_api_key,
-    authenticate_auth_token,
-    generate_auth_token,
-)
+from src.auth import generate_auth_token
+from src.db import UserDatabase, UserExistsError
+from src.config import Config
 
 
 STATIC_DIRECTORY = pathlib.Path(__file__).parent / 'static'
+CONFIG = Config()
+USER_DATABASE = UserDatabase(
+    username=CONFIG.db_username,
+    password=CONFIG.db_password,
+    project_name=CONFIG.db_project_name,
+    database_name=CONFIG.db_name,
+    collection_name=CONFIG.db_collection_name
+)
 
 
 APP = FastAPI()
@@ -39,40 +43,47 @@ for file_sys_obj in STATIC_DIRECTORY.iterdir():
     )
 
 
-# APP.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=['*'],
-#     allow_credentials=True,
-#     allow_methods=['*'],
-#     allow_headers=['*'],
-# )
-
-
 @APP.get("/")
 async def root():
-    return {"message": "Please authenticate at /auth/token to access other routes."}
+    return {"message": "Please authenticate at /login to access other routes."}
 
 
-@APP.post('/auth/token')
-async def authenticate_user(api_key: bool = Depends(authenticate_api_key)):
-    if api_key:
-        auth_token = generate_auth_token()
-        AUTH_TOKENS[auth_token] = time.time()
-        return {'auth_token': auth_token}
-    raise HTTPException(status_code=401, detail='Unauthorized')
+async def authenticate_user(username: str, password: str):
+    user = await USER_DATABASE.read_user(username)
+    if user and user.hashed_password == hashlib.sha256(password.encode()).hexdigest():
+        return user
+    return None
 
 
-@APP.middleware('http')
-async def auth_middleware(request: Request, call_next):
-    if request.url.path in ['/', '/auth/token']:
-        response = await call_next(request)
-    else:
-        if authenticate_auth_token(request, AUTH_TOKENS):
-            response = await call_next(request)
-        else:
-            response = Response(status_code=401, content='Unauthorized')
+@APP.post('/create-user')
+async def create_user(username: str, password: str):
+    try:
+        user = await USER_DATABASE.create_user(username, password)
+    except UserExistsError:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT)
+    return JSONResponse({'username': user.username})
 
-    return response
+
+@APP.post('/login')
+async def login(username: str, password: str):
+    user = await authenticate_user(username, password)
+    if user:
+        access_token = generate_auth_token({'username': user.username})
+        return {'access_token': access_token, 'token_type': 'bearer'}
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+# @APP.middleware('http')
+# async def auth_middleware(request: Request, call_next):
+#     if request.url.path in ['/', '/auth/token']:
+#         response = await call_next(request)
+#     else:
+#         if authenticate_auth_token(request, AUTH_TOKENS):
+#             response = await call_next(request)
+#         else:
+#             response = Response(status_code=401, content='Unauthorized')
+#
+#     return response
 
 
 @APP.get('/secure/data')
