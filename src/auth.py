dbaf03491a1fcbabd5dfed3d08c8d7871b198dc1
Joshua Andujar <josue@josue.codes>
2023-05-13 21:00:34 -0400

    reducing expiration time to 1 hour and adding logging

--- src/auth.py
+++ src/auth.py
@@ -1,22 +1,52 @@
-from __future__ import annotations
-
-import os
 import datetime
 
 import jwt
 
+from src.log import get_logger
+from src.config import CONFIG
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+
+LOGGER = get_logger(__name__, CONFIG.app_name)
 ALGORITHM = 'HS256'
-AUTH_TOKEN_EXPIRE_MINUTES = 600
+AUTH_TOKEN_EXPIRE_MINUTES = 60
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 
 def generate_auth_token(data: dict) -> str:
+    LOGGER.debug(f'Attempting authorization Token generation for: {data}')
     to_encode = data.copy()
     expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=AUTH_TOKEN_EXPIRE_MINUTES)
+    LOGGER.debug(f'Expiration for {data} set to {expire.strftime(DATE_FORMAT)}')
     to_encode.update({'exp': expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, CONFIG.secret_key, algorithm=ALGORITHM)
+    LOGGER.debug('Authorization Token generated')
     return encoded_jwt
+
+
+def decode_auth_token(token: str) -> dict | None:
+    try:
+        LOGGER.debug(f'Attempting to decode Token: {token}')
+        return jwt.decode(token, CONFIG.secret_key, algorithms=[ALGORITHM])
+    except jwt.InvalidTokenError:
+        LOGGER.critical(f'Token decoding failed for: {token}')
+        return None
+
+
+def token_expired(token: str) -> bool:
+    LOGGER.debug(f'Checking is Token ({token}) is expired.')
+    token = decode_auth_token(token)
+    if not token:
+        LOGGER.debug(f'Token ({token}) unable to be decoded so defaulting to "expired"')
+        return True
+    exp = token.get('exp')
+    expiration = datetime.datetime.fromtimestamp(exp)
+    current_time = datetime.datetime.now()
+    LOGGER.debug(
+        f'Token ({token}) expires at {expiration.strftime(DATE_FORMAT)} '
+        f'and current time is {current_time.strftime(DATE_FORMAT)} '
+        f':: {expiration < current_time}'
+    )
+    return expiration < current_time
 
 
 if __name__ == '__main__':  # pragma: no cover
