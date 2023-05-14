ec505c88491e9f7de0d206b212d55bda4034611f
Joshua Andujar <josue@josue.codes>
2023-05-14 14:32:05 -0400

    able to create users, login and get an auth token and access another route if that auth token is valid

--- main.py
+++ main.py
@@ -1,31 +1,70 @@
-from __future__ import annotations
-
-import hashlib
+import sys
 import pathlib
 from typing import Dict
 
 from fastapi import (
+    Depends,
     FastAPI,
     HTTPException,
-    status
+    Request,
+    status,
+)
+from fastapi.security import (
+    HTTPBearer,
+    HTTPAuthorizationCredentials,
 )
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
 import uvicorn
 
-from src.auth import generate_auth_token
-from src.db import UserDatabase, UserExistsError
-from src.config import Config
+from src.auth import (
+    generate_auth_token,
+    token_expired,
+    submitted_password_matches_database,
+)
+from src.persistence.db import Database
+from src.persistence.user import (
+    UserExistsError,
+    User,
+    create_user,
+    read_user,
+)
+from src.persistence.auth_token import (
+    create_token,
+    read_token,
+    delete_token,
+)
+from src.config import CONFIG
+from src.log import get_logger
 
 
 STATIC_DIRECTORY = pathlib.Path(__file__).parent / 'static'
-CONFIG = Config()
-USER_DATABASE = UserDatabase(
+
+LOGGER = get_logger(__name__, CONFIG.app_name)
+
+
+def log_exception(exc_type, exc_value, exc_traceback):
+    LOGGER.critical(
+        "Uncaught exception",
+        exc_info=(exc_type, exc_value, exc_traceback),
+    )
+
+
+sys.excepthook = log_exception
+
+USER_DATABASE = Database(
     username=CONFIG.db_username,
     password=CONFIG.db_password,
     project_name=CONFIG.db_project_name,
-    database_name=CONFIG.db_name,
-    collection_name=CONFIG.db_collection_name
+    database_name=CONFIG.db_name_user,
+    collection_name=CONFIG.db_collection_name_user
+)
+TOKEN_DATABASE = Database(
+    username=CONFIG.db_username,
+    password=CONFIG.db_password,
+    project_name=CONFIG.db_project_name,
+    database_name=CONFIG.db_name_token,
+    collection_name=CONFIG.db_collection_name_token
 )
 
 
@@ -43,32 +82,43 @@ for file_sys_obj in STATIC_DIRECTORY.iterdir():
     )
 
 
-@APP.get("/")
-async def root():
-    return {"message": "Please authenticate at /login to access other routes."}
-
-
-async def authenticate_user(username: str, password: str):
-    user = await USER_DATABASE.read_user(username)
-    if user and user.hashed_password == hashlib.sha256(password.encode()).hexdigest():
+async def authenticate_user(username: str, password: str) -> User | None:
+    LOGGER.debug(f'Attempting to authenticate User {username}')
+    user: User | None = await read_user(USER_DATABASE.collection, username)
+    if user and submitted_password_matches_database(password, user.hashed_password):
+        LOGGER.debug(f'User {username} authenticated')
         return user
+    LOGGER.debug(f'User {username} failed authentication')
     return None
 
 
-@APP.post('/create-user')
-async def create_user(username: str, password: str):
+@APP.post('/signup')
+async def signup(request: Request):
+    data = await request.json()
+    username = data.get('username')
+    password = data.get('password')
     try:
-        user = await USER_DATABASE.create_user(username, password)
+        user: User = await create_user(USER_DATABASE.collection, username, password)
     except UserExistsError:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT)
     return JSONResponse({'username': user.username})
 
 
 @APP.post('/login')
-async def login(username: str, password: str):
+async def login(request: Request):
+    data = await request.json()
+    LOGGER.debug(
+        f'Login data: Username - {data.get("username")} '
+        f'-- Password - {"*" if data.get("password") else "blank / missing"}'
+    )
+    username = data.get('username')
+    password = data.get('password')
     user = await authenticate_user(username, password)
     if user:
         access_token = generate_auth_token({'username': user.username})
+        LOGGER.debug('Access Token created')
+        await create_token(TOKEN_DATABASE.collection, access_token)
+        LOGGER.debug('Access Token stored')
         return {'access_token': access_token, 'token_type': 'bearer'}
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
@@ -86,14 +136,41 @@ async def login(username: str, password: str):
 #     return response
 
 
+security = HTTPBearer()
+
+
+async def verify_token(
+        credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+    if not credentials or not credentials.scheme.lower() == 'bearer':
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid authentication credentials'
+        )
+    token = credentials.credentials
+    db_token = await read_token(TOKEN_DATABASE.collection, token)
+    if not db_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid token'
+        )
+    if token_expired(token):
+        await delete_token(TOKEN_DATABASE.collection, token)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Expired token'
+        )
+    return token
+
+
 @APP.get('/secure/data')
-async def get_secure_data():
-    return {"data": "This is secure data"}
+async def get_secure_data(_: str = Depends(verify_token)):
+    return {'data': 'This is secure data'}
 
 
 if __name__ == '__main__':  # pragma: no cover
     uvicorn.run(
         app=APP,
         host='0.0.0.0',
-        port=80
+        port=8888
     )
