de91c6877076c24688ed30e8ef6ea6854e8e679e
Joshua Andujar <josue@josue.codes>
2023-05-14 14:28:47 -0400

    not sure if this is a security concern but not going to log token values.

--- src/persistence/auth_token.py
+++ src/persistence/auth_token.py
@@ -25,14 +25,14 @@ async def create_token(
         collection: pymongo.collection.Collection,
         token: str
 ) -> Token:
-    LOGGER.debug(f'Collection: {collection.name} :: Token: {token}')
+    LOGGER.debug(f'Collection: {collection.name}')
     auth_token = Token(
         _id=bson.objectid.ObjectId(),
         token=token,
     )
-    LOGGER.debug(f'Token pydantic class created: {auth_token} :: {auth_token.dict()}')
+    LOGGER.debug('Token pydantic class created')
     collection.insert_one(auth_token.dict())
-    LOGGER.debug(f'Token ({token}) inserted into Collection ({collection.name})')
+    LOGGER.debug(f'Token inserted into Collection ({collection.name})')
     return auth_token
 
 
@@ -40,12 +40,12 @@ async def read_token(
         collection: pymongo.collection.Collection,
         token: str
 ) -> Token | None:
-    LOGGER.debug(f'Read Token Attempt: {token}')
+    LOGGER.debug('Read Token attempt')
     auth_token = collection.find_one({'token': token})
     if auth_token:
-        LOGGER.debug(f'Token Found: {token}')
+        LOGGER.debug(f'Token found')
         return Token(**auth_token)
-    LOGGER.debug(f'Token ({token}) Not Found')
+    LOGGER.debug(f'Token not found')
     return None
 
 
@@ -53,9 +53,9 @@ async def delete_token(
         collection: pymongo.collection.Collection,
         token: str
 ) -> None:
-    LOGGER.debug(f'Attempting token deletion for Token ({token})')
+    LOGGER.debug('Attempting Token deletion')
     collection.delete_one(filter={'token': token})
-    LOGGER.debug(f'Token ({token}) deleted')
+    LOGGER.debug('Token deleted')
 
 
 if __name__ == '__main__':  # pragma: no cover
